9ea5749e201f03d7e277ed0b27af69434b6319ab
Aayush Gupta <aayushgupta7995@gmail.com>
2021-10-24 13:14:55 +0530

    Products detail page created

--- lib/pages/posts/post_detail.dart
+++ lib/pages/posts/post_detail.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:trendmate/models/media.dart';
 import 'package:trendmate/providers/posts_provider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class PostDetail extends StatelessWidget {
@@ -11,10 +13,17 @@ class PostDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // postIndex will get us the post we want to display
     final postIndex = ModalRoute.of(context)!.settings.arguments as int;
 
+    // loadedPost is the posts we want to display
     final loadedPost = Provider.of<PostsProvider>(context, listen: false)
         .findByIndex(postIndex);
+
+    // convert and format timeStamp to datetime
+    var dateTime = DateFormat("dd-MMMM, yyyy - h:mma").format(
+        DateTime.fromMillisecondsSinceEpoch(
+            loadedPost.dateTime.seconds * 1000));
 
     return Scaffold(
       appBar: AppBar(
@@ -27,68 +36,103 @@ class PostDetail extends StatelessWidget {
         actions: [
           IconButton(
               onPressed: () {
-                // launch(loadedPost.location),
+                // google maps will open the location when clicked on locaton icon button
+                launch(
+                    "http://maps.google.com/maps?z=12&t=m&q=loc:${loadedPost.location.latitude}+${loadedPost.location.longitude}");
               },
               icon: Icon(Icons.location_on))
         ],
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 10),
+        padding: EdgeInsets.all(10),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Slider for images and videos
+            CarouselSlider(
+              options: CarouselOptions(height: 300.0),
+              items: loadedPost.medias.map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    if (i.type == MediaType.image) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: Image.network(i.url),
+                      );
+                    } else {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: GestureDetector(
+                          onTap: () => launch(i.url),
+                          child: Icon(Icons.play_arrow),
+                        ),
+                      );
+                    }
+                  },
+                );
+              }).toList(),
+            ),
+            // show no. of shares
             Row(
               children: [
                 Icon(Icons.people),
                 Text(loadedPost.share_no.toString()),
               ],
             ),
-            // Row(children: [
-            //   Text("Tags: "),
-            //   ListView.builder(scrollDirection: Axis.horizontal, shrinkWrap: true, itemCount: loadedPost.tags.length, itemBuilder: (ctx, i) => Text(loadedPost.tags[i])),
-            // ],),
+            // author and datetime
             Text(
-              "By: ${loadedPost.by}",
+              "By: ${loadedPost.by} (${dateTime})",
               style: TextStyle(color: Colors.grey, fontSize: 15),
             ),
             SizedBox(
               height: 5,
             ),
+            // description
             Text(loadedPost.description),
             SizedBox(
               height: 5,
             ),
-            // GridView.builder(
-            //   shrinkWrap: true,
-            //     itemCount: loadedPost.medias.length,
-            //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            //       crossAxisCount: 2,
-            //       childAspectRatio: 3 / 2,
-            //       crossAxisSpacing: 10,
-            //       mainAxisSpacing: 10,
-            //     ),
-            //     itemBuilder: (ctx, i) {
-            //       if (loadedPost.medias[i].type == MediaType.image) {
-            //         return Image.network(loadedPost.medias[i].url);
-            //       } else {
-            //         return Text("url: ${loadedPost.medias[i].url}");
-            //       }
-            //     }),
             SizedBox(
               height: 10,
             ),
             Text(
-              "Products",
+              "Tags: ",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: loadedPost.tags.length,
+                  itemBuilder: (ctx, i) => Card(
+                      color: Colors.blue,
+                      child: Text(loadedPost.tags[i],
+                          style: TextStyle(color: Colors.white)))),
+            ),
+            Text(
+              "Products: ",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(
               height: 10,
             ),
-            // ListView.builder(
-            //   itemCount: loadedPost.products.length,
-            //   itemBuilder: (ct, idx) => Text(loadedPost.products[idx]),
-            // ),
+            SizedBox(
+              height: 50,
+              child: ListView.builder(
+                itemCount: loadedPost.products.length,
+                itemBuilder: (ct, idx) => Text(loadedPost.products[idx]),
+              ),
+            )
           ],
         ),
       ),
--- lib/providers/posts_provider.dart
+++ lib/providers/posts_provider.dart
@@ -18,12 +18,23 @@ class PostsProvider with ChangeNotifier {
             type: MediaType.image,
             url:
                 "https://cdn.pixabay.com/photo/2016/05/20/17/17/social-media-1405601_1280.png",
-            title: "media-1")
+            title: "media-1"),
+        Media(
+            mediaId: '2',
+            type: MediaType.image,
+            url: "https://bit.ly/3GiLVar",
+            title: "media-2"),
+        Media(
+            mediaId: '3',
+            type: MediaType.video,
+            url: "https://www.youtube.com/watch?v=WgU7P6o-GkM",
+            title: "media-3"),
       ],
       location: GeoPoint(4.5, 4.5),
       share_no: 5,
-      tags: ['t-shirt', 'red'],
-      description: 'Hello, world!',
+      tags: ['tag-1', 'tag-2', 'tag-3333'],
+      description:
+          "Fiction-writing also has modes: action, exposition, description, dialogue, summary, and transition.[4] Author Peter Selgin refers to methods, including action, dialogue, thoughts, summary, scenes, and description.[5] Currently, there is no consensus within the writing community regarding the number and composition of fiction-writing modes and their uses. Description is the fiction-writing mode for transmitting a mental image of the particulars of a story. Together with dialogue, narration, exposition, and summarization, description is one of the most widely recognized of the fiction-writing modes. As stated in Writing from A to Z, edited by Kirk Polking, description is more than the amassing of details; it is bringing something to life by carefully choosing and arranging words and phrases to produce the desired effect.[6] The most appropriate and effective techniques for presenting description are a matter of ongoing discussion among writers and writing coaches.",
       products: ['product-1', 'product-2'],
     ));
   }
