ba6331ecbb955c729142da4c8f3a5b936b9fdef3
avneeshg03 <goyalavi03@gmail.com>
2021-10-30 10:46:00 +0530

    Removed otp

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:trendmate/pages/auth/login_page.dart';
 import 'package:trendmate/pages/auth/signup_page.dart';
-import 'package:trendmate/pages/auth/otp_page.dart';
 import 'package:trendmate/pages/boards/board_detail.dart';
 import 'package:trendmate/pages/boards/boards_page.dart';
 import 'package:trendmate/pages/posts/post_detail.dart';
