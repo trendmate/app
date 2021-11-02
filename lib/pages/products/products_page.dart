d152c539f52d4a82d9f336c08c610642252bf886
Aayush Gupta <aayushgupta7995@gmail.com>
2021-11-02 14:57:43 +0530

    Bugs in favorites removed

--- lib/pages/products/products_page.dart
+++ lib/pages/products/products_page.dart
@@ -57,7 +57,82 @@ class _ProductsPageState extends State<ProductsPage> {
                       ? await launch(_url)
                       : throw 'Could not launch $_url';
                 },
-                child: ProductWidget(idx),
+                // child: ProductWidget(idx),
+                child: Card(
+                  child: Container(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              //alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Image.network(
+                                productsProvider.products[idx].image,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.all(8),
+                            child: Text(productsProvider.products[idx].title),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(left: 10, bottom: 10),
+                                child: Text(
+                                  "Rs. ${productsProvider.products[idx].price.toString()}",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blue[600]),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                      child: IconButton(
+                                    icon: Icon(Icons.library_add),
+                                    onPressed: () {
+                                      // TODO add remove collection dialog
+                                      // return _createAddRemoveBuilders(context, productsProvider,
+                                      //     boardsProvider, idx);
+                                    },
+                                  )),
+                                  Container(
+                                    child: IconButton(
+                                        color: Colors.red,
+                                        icon: Icon(productsProvider.favoritesSet
+                                                .contains(productsProvider
+                                                    .products[idx].productId)
+                                            ? Icons.favorite
+                                            : Icons.favorite_border),
+                                        onPressed: () {
+                                          if (productsProvider.favoritesSet
+                                              .contains(productsProvider
+                                                  .products[idx].productId)) {
+                                            productsProvider.removeFavorites(
+                                                productsProvider
+                                                    .products[idx].productId);
+                                          } else {
+                                            productsProvider.addFavorites(
+                                                productsProvider
+                                                    .products[idx].productId);
+                                          }
+                                        }),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )
+                        ]),
+                  ),
+                ),
               );
             },
           );
