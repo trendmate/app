2969d7fe304cc32f988db85e878897c378e5fef5
Abhinav Marwaha <abhinavmarwaha.bt19cse@pec.edu.in>
2021-11-23 11:51:30 +0530

    favourites bug fixes

--- lib/pages/favourites/favourites_page.dart
+++ lib/pages/favourites/favourites_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:trendmate/models/ecom/product.dart';
 import 'package:trendmate/providers/products_provider.dart';
+import 'package:trendmate/services/firebase_methods.dart';
 
 class FavouritesPage extends StatefulWidget {
   static const routeName = '/favourites-page';
@@ -37,78 +39,88 @@ class _FavouritesPageState extends State<FavouritesPage> {
                   childAspectRatio: 0.5),
               itemCount: productsProvider.favorites.length,
               itemBuilder: (ctx, idx) {
-                var product = productsProvider.products.firstWhere((element) =>
-                    element.productId == productsProvider.favorites[idx]);
+                return FutureBuilder<Product>(
+                    future: FirebaseMethods.instance
+                        .getProduct(productsProvider.favorites[idx]),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) return CircularProgressIndicator();
+                      final product = snapshot.data!;
 
-                return Card(
-                  child: Container(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Container(
-                              //alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(16),
-                              ),
-                              child: Image.network(
-                                // TODO
-                                product.image,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.all(8),
-                            // TODO
-                            child: Text(product.title),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                padding: EdgeInsets.only(left: 10, bottom: 10),
-                                child: Text(
-                                  // TODO
-                                  "Rs. ${product.price.toString()}",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.blue[600]),
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Container(
-                                    child: IconButton(
-                                        color: Colors.red,
-                                        icon: Icon(productsProvider.favoritesSet
-                                                .contains(productsProvider
-                                                    .favorites[idx])
-                                            ? Icons.favorite
-                                            : Icons.favorite_border),
-                                        onPressed: () {
-                                          if (productsProvider.favoritesSet
-                                              .contains(productsProvider
-                                                  .favorites[idx])) {
-                                            productsProvider.removeFavorites(
-                                                productsProvider
-                                                    .favorites[idx]);
-                                          } else {
-                                            productsProvider.addFavorites(
-                                                productsProvider
-                                                    .favorites[idx]);
-                                          }
-                                        }),
+                      return Card(
+                        child: Container(
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Container(
+                                    //alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    child: Image.network(
+                                      // TODO
+                                      product.image,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                ],
-                              ),
-                            ],
-                          )
-                        ]),
-                  ),
-                );
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  padding: EdgeInsets.all(8),
+                                  // TODO
+                                  child: Text(product.title),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      padding:
+                                          EdgeInsets.only(left: 10, bottom: 10),
+                                      child: Text(
+                                        // TODO
+                                        "Rs. ${product.price.toString()}",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.blue[600]),
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Container(
+                                          child: IconButton(
+                                              color: Colors.red,
+                                              icon: Icon(productsProvider
+                                                      .favoritesSet
+                                                      .contains(productsProvider
+                                                          .favorites[idx])
+                                                  ? Icons.favorite
+                                                  : Icons.favorite_border),
+                                              onPressed: () {
+                                                if (productsProvider
+                                                    .favoritesSet
+                                                    .contains(productsProvider
+                                                        .favorites[idx])) {
+                                                  productsProvider
+                                                      .removeFavorites(
+                                                          productsProvider
+                                                              .favorites[idx]);
+                                                } else {
+                                                  productsProvider.addFavorites(
+                                                      productsProvider
+                                                          .favorites[idx]);
+                                                }
+                                              }),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ]),
+                        ),
+                      );
+                    });
               }),
     );
   }
